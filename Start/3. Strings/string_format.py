1957c3986118bc5a3b5e7e5d62459129b25c87ad
Alexandra Ferrucci <36905791+aliferrucci@users.noreply.github.com>
2025-06-17 05:12:55 +0000

    Completed part 3 exercises

--- Start/3. Strings/string_format.py
+++ Start/3. Strings/string_format.py
@@ -2,6 +2,10 @@
 # Formatting output strings
 
 # Basic formatting - center(), ljust(), rjust()
+width = 40
+# print("center".center(width, '-'))
+# print("left".ljust(width, '-'))
+# print("right".rjust(width, '-'))
 
 # Formatting strings with format specification codes
 # Format spec is: [[fill]align][sign]["z"]["#"]["0"][width][grouping_option]["."precision][type]
@@ -16,18 +20,35 @@ val4 = -280.7
 # print(f"{val4}")
 
 # Specify a precision and type
-
+# print(f"{val1:.2f}")
+# print(f"{val2:.2f}")
+# print(f"{val3:.2f}")
+# print(f"{val4:.2f}")
 
 # Use alignment and width and leading zeros
 # < is left align, > is right align, ^ is centered
-
+# print(f"{val1:<010.2f}")
+# print(f"{val2:>010.2f}")
+# print(f"{val3:^010.2f}")
+# print(f"{val4:>010.2f}")
 
 # Use a grouping option and +/- signs
-
+# print(f"{val1:>-10,.2f}")
+# print(f"{val2:>-10,.2f}")
+# print(f"{val3:>+10,.2f}")
+# print(f"{val4:>+10,.2f}")
 
 # Insert a fill character
-
+# print(f"{val1:_>-10,.2f}")
+# print(f"{val2:_>-10,.2f}")
+# print(f"{val3:_>+10,.2f}")
+# print(f"{val4:_>+10,.2f}")
 
 # Create format specifiers dynamically
 width = 10
 precision = 2
+format_spec = f"{123.456:{width}.{precision}f}"
+print(format_spec)
+
+format_spec = "{val:{width}.{precision}f}".format(val=val2,width=10,precision=2)
+print(format_spec)
--- Start/3. Strings/string_search.py
+++ Start/3. Strings/string_search.py
@@ -2,24 +2,35 @@
 
 
 sample_text = "The quick brown fox jumps over the lazy dog."
+tempstr= "the quick brown fox jumps over the lazy dog."
 
 # Using find() to find the first occurrence of a substring
-
+print("First occurrence of 'the':", sample_text.find("the"))
+# to make this case insensitive set text to either upper/lower case
 
 # Example with optional start and end parameters
+print("First occurrence of 'the':", sample_text.find("the", 5, 36))
 
 
 # Using index() to find the first occurrence of a substring (raises ValueError if not found)
+print("First occurrence of 'fox':", sample_text.index("fox"))
 
+try:
+  print("First occurrence of 'fax':", sample_text.index("fax"))
+except ValueError:
+  print("Not found")
 
 # The 'in' operator can be used for Boolean testing:
-
+print("Is 'fox' present:", "fox" in sample_text)
 
 # Using rfind() to find the last occurrence of a substring
-
+print("Last occurrence of 'the':", sample_text.rfind('the'))
 
 # Using rindex() to find the last occurrence of a substring (raises ValueError if not found)
-
+print("Last occurrence of 'jump':", sample_text.rindex('jump'))
 
 # The replace() function will find content in the string and replace it
-
+result = sample_text.replace("lazy", "tired")
+print(result) # string are immmutable so new strings are being created
+result = tempstr.replace("the", "THE")
+print(result)
