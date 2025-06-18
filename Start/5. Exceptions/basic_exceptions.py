49bec1a19bc571e7b416a44399b4fbf9977ad90f
Alexandra Ferrucci <36905791+aliferrucci@users.noreply.github.com>
2025-06-18 01:27:27 +0000

    Completed all exercises and added directory/files for example

--- Start/5. Exceptions/basic_exceptions.py
+++ Start/5. Exceptions/basic_exceptions.py
@@ -2,9 +2,19 @@
 # Working with basic exception handling
 
 # Try to execute some code that might cause an exception:
-
-num = input("Enter the first number: ")
-denom = input("Enter the second numnber: ")
-n = int(num)
-d = int(denom)
-print(n/d)
+try:
+  num = input("Enter the first number: ")
+  denom = input("Enter the second numnber: ")
+  n = int(num)
+  d = int(denom)
+  result = n/d
+except ZeroDivisionError as e:
+  print("You can't divide by zero!")
+  print(e)
+except ValueError as e:
+  print("Must enter a valid number")
+  print(e)
+else:
+  print(result)
+finally:
+  print("Thanks for playing!")
--- Start/5. Exceptions/common_exceptions.py
+++ Start/5. Exceptions/common_exceptions.py
@@ -3,15 +3,20 @@
 
 # IndexError occurs when you try to access an index that is out of range
 int_list = [0,3,6,1,8,7,3,5]
-
+print(int_list[10]) # Raise IndexError
+print(int_list["a"]) # Raise TypeError
 
 # KeyError is similar - it is raised when a key is not found in a Dictionary
 my_dict = {1: "one", 2: "two", 3: "three"}
-
+print(my_dict[4]) # Raise KeyError
 
 # FileNotFoundError is raised when you try to access a file that doesn't exist
-
+with open("file_doesnt_exist.txt", "r") as f:
+  print(f) # Raise FileNotFoundError
 
 # FileExistsError is raised when a file or directory already exists
+import os
+os.mkdir("testdir") # Raise FileExistsError
 
 # NotADirectoryError is raised when try to perform a dir operation on a non-dir object
+os.listdir("myfile.txt") # Raise NotADirectoryError
