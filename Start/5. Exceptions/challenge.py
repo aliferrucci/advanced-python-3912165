7564a6b0e0e22309055e16ddf6fe38514055a93c
Alexandra Ferrucci <36905791+aliferrucci@users.noreply.github.com>
2025-06-18 01:37:01 +0000

    Completed final challenage

--- Start/5. Exceptions/challenge.py
+++ Start/5. Exceptions/challenge.py
@@ -2,13 +2,19 @@
 # Programming challenge for working with Exceptions
 
 # Implement the InvalidTempError exception class here
-
+class InvalidTempError(Exception):
+    """Raise exception when temperature is out of range below 100 or above 500 degrees"""
+    def __init__(self, temp):
+        message = f"Invalid temperature setting: {temp}. Must be between 100 and 500 degrees or zero"
+        super().__init__(message)
 
 class DigitalOven:
     def __init__(self):
         self.temp = 0
 
     def set_temp(self, temp):
+        if 0 < temp < 100 or temp > 500:
+            raise InvalidTempError(temp)
         self.temp = temp
 
     def get_temp(self):
@@ -18,6 +24,10 @@ def test_oven(test_temp):
     global oven
     try:
         oven.set_temp(test_temp)
+    except InvalidTempError as e:
+        print(f"Error: {e}")
+    else:
+        print(f"Temperature was set to {test_temp} degrees")
     finally:
         print(f"Current temp setting is {oven.get_temp()}")
 
